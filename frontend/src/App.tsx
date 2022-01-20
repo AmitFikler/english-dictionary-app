32acdb8f46868fe542007eeee72a15259883972f
AmitFikler <amitfikler@gmail.com>
2022-01-20 19:09:33 +0200

    📁 Build Search component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import SearchInput from './components/Search';
 
 function App() {
   return (
     <div className="App">
       <h1 className="title">Dictionary</h1>
+      <SearchInput />
     </div>
   );
 }
---
+++ frontend/src/components/Search.tsx
@@ -0,0 +1,10 @@
+function SearchInput() {
+  return (
+    <div style={{ marginTop: '10%' }}>
+      <input className="search" type={'text'}></input>
+      <button className="btn-search">Search</button>
+    </div>
+  );
+}
+
+export default SearchInput;
