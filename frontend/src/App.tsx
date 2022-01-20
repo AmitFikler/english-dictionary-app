0275a2f19be69dc3384e68649d1d30d90912dcda
AmitFikler <amitfikler@gmail.com>
2022-01-20 19:58:18 +0200

    ✨Implement dynamic route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css';
 import SearchInput from './components/Search';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Word from './components/Word';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1 className="title">Dictionary</h1>
       <SearchInput />
-    </div>
+      <Router>
+        <Routes>
+          <Route path={`/:word`} element={<Word />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
