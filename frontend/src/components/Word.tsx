f24ab586f220f5b07cded97044c0c1abc7fd663f
AmitFikler <amitfikler@gmail.com>
2022-01-20 20:50:36 +0200

    📁 Added Word api request

--- frontend/src/components/Word.tsx
+++ frontend/src/components/Word.tsx
@@ -1,10 +1,48 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { WordObj } from '../../@types/wordtype';
 
 function Word() {
   const { word } = useParams();
+  const [definition, setDefinition] = useState<WordObj[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    getDefinition();
+  }, []);
+
+  const getDefinition = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/word/${word}`);
+      setDefinition(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <h1>{word}</h1>
+      {isLoading ? <span className="loader"></span> : ''}
+      {definition.map((word) => {
+        console.log(word.definitions);
+        return (
+          <div>
+            <h1>
+              {word.word}-<i>{word.pos}</i>
+            </h1>
+            {word.definitions.map((defin, i) => (
+              <div>
+                <p>
+                  {i + 1}.{defin}
+                </p>
+                <br />
+              </div>
+            ))}
+          </div>
+        );
+      })}
     </>
   );
 }
