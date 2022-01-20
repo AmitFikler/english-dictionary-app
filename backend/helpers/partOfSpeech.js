3dbf64582fc9c42332fd7b50407b17e91060afa6
AmitFikler <amitfikler@gmail.com>
2022-01-20 15:56:28 +0200

    🛠 create helpers part of speech

---
+++ backend/helpers/partOfSpeech.js
@@ -0,0 +1,10 @@
+exports.PART_OF_SPEECH_DICT = {
+  adjectives: 'a.',
+  adverbs: 'adv.',
+  interjections: 'interj.',
+  nouns: 'n.',
+  verbs: 'v.',
+  pronouns: 'pron.',
+  prepositions: 'prep.',
+  conjunctions: 'conj.',
+};
