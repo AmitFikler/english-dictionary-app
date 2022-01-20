e31958bce83da93194bf77509176ee5bd8a68e3f
AmitFikler <amitfikler@gmail.com>
2022-01-20 16:14:54 +0200

    🛠 build controller word and implement findWordWithPos

--- backend/controllers/word.js
+++ backend/controllers/word.js
@@ -1,4 +1,13 @@
-exports.findWord = (req, res) => {
+const AWS = require('aws-sdk');
+const { PART_OF_SPEECH_DICT } = require('../helpers/partOfSpeech');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient({
+  region: 'us-east-1',
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+});
+
+exports.findWord = async (req, res) => {
   const { word } = req.params;
   const params = {
     TableName: 'english-dict',
@@ -7,7 +16,29 @@ exports.findWord = (req, res) => {
       '#word': 'word',
     },
     ExpressionAttributeValues: {
-      ':word': word,
+      ':word': word.toUpperCase(),
+    },
+  };
+  try {
+    const words = await dynamoDB.query(params).promise();
+    res.send(words.Items);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+exports.findWordWithPos = async (req, res) => {
+  const { word, partOfSpeech } = req.params;
+  const params = {
+    TableName: 'english-dict',
+    KeyConditionExpression: '#word = :word AND #pos = :pos',
+    ExpressionAttributeNames: {
+      '#word': 'word',
+      '#pos': 'pos',
+    },
+    ExpressionAttributeValues: {
+      ':word': word.toUpperCase(),
+      ':pos': PART_OF_SPEECH_DICT[partOfSpeech.toLowerCase()],
     },
   };
   try {
--- backend/routers/word.js
+++ backend/routers/word.js
@@ -1,34 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const AWS = require('aws-sdk');
-const { findWord } = require('../controllers/word');
-
-const dynamoDB = new AWS.DynamoDB.DocumentClient({
-  region: 'us-east-1',
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-});
+const { findWord, findWordWithPos } = require('../controllers/word');
 
 router.get('/:word', findWord);
 
-// router.get('/:word', async (req, res) => {
-//   const { word, partOfSpeech } = req.params;
-//   const params = {
-//     TableName: 'english-dict',
-//     KeyConditionExpression: '#word = :word and #',
-//     ExpressionAttributeNames: {
-//       '#word': 'word',
-//     },
-//     ExpressionAttributeValues: {
-//       ':word': word,
-//     },
-//   };
-//   try {
-//     const words = await dynamoDB.query(params).promise();
-//     res.send(words.Items);
-//   } catch (error) {
-//     res.send(error);
-//   }
-// });
+router.get('/:word/:partOfSpeech', findWordWithPos);
 
 module.exports = router;
