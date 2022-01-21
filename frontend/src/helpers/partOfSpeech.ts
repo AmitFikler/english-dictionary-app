63a6287ac990ae960582f6d2cbb719aacdcf8df5
AmitFikler <amitfikler@gmail.com>
2022-01-21 14:58:57 +0200

    🩹 Minor fix on PART_OF_SPEECH_DICT

--- frontend/src/helpers/partOfSpeech.ts
+++ frontend/src/helpers/partOfSpeech.ts
@@ -1,10 +1,10 @@
 export const PART_OF_SPEECH_DICT = [
-  'adjectives',
-  'adverbs',
-  'interjections',
-  'nouns',
-  'verbs',
-  'pronouns',
-  'prepositions',
-  'conjunctions',
+  'Adjectives',
+  'Adverbs',
+  'Interjections',
+  'Nouns',
+  'Verbs',
+  'Pronouns',
+  'Prepositions',
+  'Conjunctions',
 ];
