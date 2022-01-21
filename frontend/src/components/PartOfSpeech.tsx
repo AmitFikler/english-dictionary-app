8667a4cea27d3c75190e089a0253ba9307d2c916
AmitFikler <amitfikler@gmail.com>
2022-01-21 15:48:41 +0200

    ✨ rolling random part of speech word

--- frontend/src/components/PartOfSpeech.tsx
+++ frontend/src/components/PartOfSpeech.tsx
@@ -1,36 +1,81 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { WordObj } from '../../@types/wordtype';
 import { PART_OF_SPEECH_DICT } from '../helpers/partOfSpeech';
 
 function PartOfSpeechComp() {
   const { partOfSpeech } = useParams();
   const navigate = useNavigate();
-  //   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [definition, setDefinition] = useState<WordObj>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (partOfSpeech) {
-      if (!PART_OF_SPEECH_DICT.includes(partOfSpeech)) {
-        navigate('/');
-      }
+      getRandomDefinition(partOfSpeech);
     }
   }, []);
 
-  // const getDefinition = async () => {
-  //   try {
-  //     const response = await axios.get(`http://localhost:3001/word/${/part-of-speech}`);
-  //     setDefinition(response.data);
-  //     setIsLoading(false);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const getRandomDefinition = async (part: string) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/part-of-speech/${part}`
+      );
+      setDefinition(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkDefinition = () => {
+    if (definition) {
+      return (
+        <div>
+          <h1>
+            {definition.word}-<i>{definition.pos}</i>
+          </h1>
+          {definition.definitions.map((defin, i) => (
+            <div className="word-def ">
+              <p>
+                {i + 1}.{' '}
+                {defin.split(' ').map((word) => {
+                  return (
+                    <span
+                      onClick={() => {
+                        navigating(word);
+                      }}
+                    >
+                      {word}
+                      {'  '}
+                    </span>
+                  );
+                })}
+              </p>
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>The word does not exist in our dictionary</h2>
+          <h4>Try another word.</h4>
+        </div>
+      );
+    }
+  };
+
+  const navigating = (word: string) => {
+    if (word) {
+      const cleanWord = word.replace(/[^a-zA-Z ]/g, '');
+      setIsLoading(true);
+      navigate('/word/' + cleanWord + '/All');
+    }
+  };
 
   return (
-    <>
-      {/* {isLoading ? <span className="loader"></span> : ''} */}
-      <h2>{partOfSpeech}:</h2>
-    </>
+    <>{isLoading ? <span className="loader"></span> : checkDefinition()}</>
   );
 }
 
